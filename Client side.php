9257d2df7573ca61460687692cd74da9b39988cd
medali8 <mohamedali.saidi@esen.tn>
2023-04-17 11:16:35 +0000

    changes

---
+++ Client side.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css1/style4.css">
+    
+</head>
+<section id="profil">
+    <div class="conatiner">
+        <div class="row">
+            <div class="sidebar col-1">
+                <ul>
+                    <li><a href=""><i class="bi bi-house-door"></i></a></li>
+                    <li><a href=""><i class="bi bi-archive"></i></a></li>
+                    <li><a href=""><i class="bi bi-person"></i></a></li>
+                    <li><a href=""><i class="bi bi-gear-fill"></i></a></li>
+                
+                <br><br><br><br><br>
+                    <li><a href=""><i class="bi bi-door-open"></i></a></li>
+               
+                    </ul>
+                <br><br>
+            </div>
+            <div class="col-11">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3 text-begin">
+                            <img src="./assets/new-logo.png" class="img-fluid">
+                        </div>
+                    </div>
+                    <br><br>
+                    <div class="row">
+                        <div class="wlcm col-lg-8 col-sm-12">
+                            <div class="card">
+                                <div class="card-body">
+                                Bienvenue, <span style="color:#04B400;"> Chér XxxxXXx </span> <br>
+                                J'espère que vous étes en forme et bonne santé pour prochaine match 
+                                </div>
+                            </div>
+                           
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
